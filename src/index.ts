ad4e9229f6ac967311e9a7a225d2758597eecb5a
gaoshuai <gaoshuai1@rongcloud.cn>
2025-07-23 10:59:24 +0800

    refactor code

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,32 @@
-import dayjs from 'dayjs'
-import duration from 'dayjs/plugin/duration'
-import isBetween from 'dayjs/plugin/isBetween'
-import isoWeek from 'dayjs/plugin/isoWeek'
+import dayjs from "dayjs";
+import duration from "dayjs/plugin/duration";
+import isBetween from "dayjs/plugin/isBetween";
+import isoWeek from "dayjs/plugin/isoWeek";
 
-dayjs.extend(duration)
-dayjs.extend(isBetween)
-dayjs.extend(isoWeek)
+dayjs.extend(duration);
+dayjs.extend(isBetween);
+dayjs.extend(isoWeek);
 
-export { default as dayjs } from 'dayjs';
+export { default as dayjs } from "dayjs";
 
-export * from './commonStyles'
-export * from './components/Calendar'
-export * from './components/CalendarBody'
-export * from './components/CalendarBodyForMonthView'
-export * from './components/CalendarEvent'
-export * from './components/CalendarEventForMonthView'
-export * from './components/CalendarHeader'
-export * from './components/CalendarHeaderForMonthView'
-export * from './components/DefaultCalendarEventRenderer'
-export * from './components/HourGuideCell'
-export * from './components/HourGuideColumn'
-export * from './components/Schedule'
-export * from './interfaces'
-export * from './theme/ThemeContext'
-export * from './theme/ThemeInterface'
-export * from './theme/defaultTheme'
-export * from './utils/datetime'
-export * from './utils/object'
-export * from './utils/react'
-export * from './utils/utility-types'
-export * from './hooks/useCalendarTouchableOpacityProps'
-export * from './hooks/useNow'
-export * from './hooks/usePanResponder'
+export * from "./commonStyles";
+export * from "./components/CalendarBody";
+export * from "./components/CalendarEvent";
+export * from "./components/CalendarEventForMonthView";
+export * from "./components/CalendarHeader";
+export * from "./components/CalendarHeaderForMonthView";
+export * from "./components/DefaultCalendarEventRenderer";
+export * from "./components/HourGuideCell";
+export * from "./components/HourGuideColumn";
+export * from "./components/Schedule";
+export * from "./interfaces";
+export * from "./theme/ThemeContext";
+export * from "./theme/ThemeInterface";
+export * from "./theme/defaultTheme";
+export * from "./utils/datetime";
+export * from "./utils/object";
+export * from "./utils/react";
+export * from "./utils/utility-types";
+export * from "./hooks/useCalendarTouchableOpacityProps";
+export * from "./hooks/useNow";
+export * from "./hooks/usePanResponder";
