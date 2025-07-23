c77bd251ae075795d866299f592df669788b5510
gaoshuai <gaoshuai1@rongcloud.cn>
2025-07-23 14:17:11 +0800

    change index

--- expo-demo/app/index.tsx
+++ expo-demo/app/index.tsx
@@ -1,34 +1,47 @@
-import { router } from 'expo-router'
-import { Button, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
+import { router } from "expo-router";
+import { useEffect } from "react";
+import {
+  Button,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 const EXAMPLES = [
   {
-    name: 'Calendar Manager',
-    path: 'CalendarManager',
+    name: "Calendar Manager",
+    path: "CalendarManager",
   },
   {
-    name: 'Calendar',
-    path: 'CalendarDemo',
+    name: "Calendar",
+    path: "CalendarDemo",
   },
   {
-    name: 'Month view pager',
-    path: 'MonthViewPagerPage',
+    name: "Month view pager",
+    path: "MonthViewPagerPage",
   },
   {
-    name: '3-days view pager',
-    path: 'ThreeDaysViewPagerPage',
+    name: "3-days view pager",
+    path: "ThreeDaysViewPagerPage",
   },
   {
-    name: 'Month view infinite pager',
-    path: 'MonthViewInfinitePagerTest',
+    name: "Month view infinite pager",
+    path: "MonthViewInfinitePagerTest",
   },
   {
-    name: 'Month View (对比)',
-    path: 'expandlbe/components/MonthView',
+    name: "Month View (对比)",
+    path: "expandlbe/components/MonthView",
   },
-]
+];
 
 export default function App() {
+  useEffect(() => {
+    setTimeout(() => {
+      router.replace("/CalendarManager");
+    });
+  }, []);
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
@@ -39,27 +52,30 @@ export default function App() {
         <View style={styles.list}>
           {EXAMPLES.map((e) => (
             <View key={e.path} style={styles.button}>
-              <Button title={e.name} onPress={() => router.push(e.path as any)} />
+              <Button
+                title={e.name}
+                onPress={() => router.push(e.path as any)}
+              />
             </View>
           ))}
         </View>
       </ScrollView>
     </SafeAreaView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   header: {
     padding: 24,
-    backgroundColor: '#eee',
+    backgroundColor: "#eee",
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   subtitle: {
     marginTop: 8,
@@ -71,4 +87,4 @@ const styles = StyleSheet.create({
   button: {
     marginVertical: 8,
   },
-})
+});
